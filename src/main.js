ef2452a284cbe1d8b96c4add83b041da3ee30059
Ricardo Conceição <ricardo220893@gmail.com>
2019-07-19 23:47:05 -0300

    - Add husky

--- src/main.js
+++ src/main.js
@@ -1,5 +1,1 @@
-a = 3;
-const b = 2
-const b = 4
-
-const c = "apsndçasdkansdkaakjsndkjansdkjnaskjdnakjsndkajsndkjasndkjansdkjansdkjasndkjjnaskdkajsndkajsnddjkasbjabsdkjbasjkdbaksjdbkajsbdkjasbdkajsbdkajsbdkajsbdkajsbdkajsbdkajsbdkjabsdkjab
+a = 0
